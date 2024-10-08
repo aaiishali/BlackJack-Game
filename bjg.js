55144244aa988196080fc56683e0803636d37a11
ilmaali <160714251+aaiishali@users.noreply.github.com>
2024-10-08 22:52:51 +0530

    Add bjg.js

---
+++ bjg.js
@@ -0,0 +1,82 @@
+let player = {                 // Objects - store data in-depth - composite / complex data type
+    name: "Aaish",             // key-value pairs
+    chips: 200
+}
+let cards = []  // array - ordered list of items
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ":$" + player.chips     //with dot . we represent the key
+
+
+function getRandomCard(){
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if(randomNumber > 10){
+        return 10
+    }
+    else if (randomNumber === 1){
+        return 11
+    } 
+    else {
+        return randomNumber
+    }
+}
+
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secCard = getRandomCard()
+    cards = [firstCard, secCard]
+    sum = firstCard + secCard
+
+
+    renderGame()
+}
+
+function renderGame(){
+
+    cardsEl.textContent = "Card: " 
+
+    for(let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = "Sum: " + sum
+     
+    if (sum <= 20){
+         message = "Do you want to draw a new card?"
+    }
+    else if (sum === 21){
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    }
+    else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    
+   messageEl.textContent = message 
+}
+
+
+function newCard(){
+
+    if(isAlive === true && hasBlackJack === false ){
+        let card = getRandomCard()
+    sum += card
+
+    cards.push(card)
+
+    renderGame()
+    }
+
+}
+
